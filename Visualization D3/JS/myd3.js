16c6083fae5870bf2b26b373d36bae56bc4f2c15
Chris Daly <chrisdaly1988@gmail.com>
2016-08-03 16:00:56 +0100

    Working on legend

--- Visualization D3/JS/myd3.js
+++ Visualization D3/JS/myd3.js
@@ -1,11 +1,24 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
+var colors = {
+  "England":  "#6a3d9a",
+  "France": "#1f78b4",
+  "Germany": "#ffd92f",
+  "Italy": "#33a02c",
+  "Spain": "#e31a1c"
+};
+
+var seasons = {
+  "0": "2008/2009",
+  "1": "2009/2010",
+  "2": "2010/2011",
+  "3": "2011/2012",
+  "4": "2012/2013",
+  "5": "2013/2014",
+  "6": "2014/2015",
+  "7": "2015/2016"};
+
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
-
-// setup x
-var xValue = function(d) { return d.X;}
-var yValue = function(d) { return d.Entropy;}
-
 
 var x = d3.scale.linear()
     .range([0, width]);
@@ -13,11 +26,12 @@ var x = d3.scale.linear()
 var y = d3.scale.linear()
     .range([height, 0]);
 
-var color = d3.scale.category10();
-
 var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("bottom");
+    .orient("bottom")
+    .tickFormat(function(d) {
+      return seasons[d]
+    })
 
 var yAxis = d3.svg.axis()
     .scale(y)
@@ -33,6 +47,13 @@ var svg = d3.select("body").append("svg")
 var tooltip = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
+//
+// var legend = svg.append("g")
+//     .attr("class", "legend")
+//     .attr("x", 100  )
+//     .attr("y", 100)
+//     .attr("height", 200)
+//     .attr("width", 200)
 
 d3.csv("../Data/teams_entropy.csv", function(error, data) {
   if (error) throw error;
@@ -42,7 +63,7 @@ d3.csv("../Data/teams_entropy.csv", function(error, data) {
     d.X = +d.X;
   });
 
-  x.domain(d3.extent(data, function(d) { return d.X; })).nice();
+  x.domain(d3.extent(data, function(d) { return d.X; })).nice(); // Make axis end in round number.
   y.domain(d3.extent(data, function(d) { return d.Entropy; })).nice();
 
   svg.append("g")
@@ -51,9 +72,9 @@ d3.csv("../Data/teams_entropy.csv", function(error, data) {
       .call(xAxis)
     .append("text")
       .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
+      .attr("x", width/2)
+      .attr("y", 30)
+      .style("text-anchor", "middle")
       .text("Season");
 
   svg.append("g")
@@ -62,10 +83,11 @@ d3.csv("../Data/teams_entropy.csv", function(error, data) {
     .append("text")
       .attr("class", "label")
       .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Entropy")
+      .attr("y", -35)
+      .attr("x", -height/2)
+      .style("text-anchor", "middle")
+      .text("Entropy (H)")
+
 
   svg.selectAll(".dot")
       .data(data)
@@ -74,22 +96,48 @@ d3.csv("../Data/teams_entropy.csv", function(error, data) {
       .attr("r", 3.5)
       .attr("cx", function(d) { return x(d.X); })
       .attr("cy", function(d) { return y(d.Entropy); })
-      .style("fill", function(d) { return color(d.Colour); })
-      .attr("outline", "None")
-      .attr("opacity", .5)
+      .style("fill", function(d) { return colors[d.League]; })
       .on("mouseover", function(d) {
           tooltip.transition()
                .duration(200)
-               .style("opacity", .9);
-          tooltip.html(d["Team"] + "<br/> (" + d["Season"]
-	        + ", " + d["Entropy"] + ")")
-               .style("left", (d3.event.pageX + 5) + "px")
-               .style("top", (d3.event.pageY - 28) + "px");
+               .style("opacity", .9)
+
+          tooltip.html("<b>"+ d["Team"] + "</b>" + "<br/>" +
+                      d["League"] + "<br/>" +
+                      d["Season"] + "<br/>" +
+	                    "H = " + Math.round(d["Entropy"] * 100) / 100)
+               .style("left", (d3.event.pageX + 20) + "px")
+               .style("top", (d3.event.pageY - 30) + "px")
+               .style("background", colors[d.League]);
       })
       .on("mouseout", function(d) {
           tooltip.transition()
                .duration(500)
                .style("opacity", 0);
       });
+
+    // draw legend
+    var legend = svg.selectAll(".legend")
+        .data(Object.keys(colors))
+      .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(40," + 20+(i * 20) + ")"; });
+
+    // draw legend text
+    legend.append("text")
+        .attr("x", 15)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "start")
+        .text(function(d) { return d;})
+
+    // draw legend colored rectangles
+    legend.append("circle")
+        .attr("class", "dot")
+        .attr("r", 3.5)
+        .attr("cx", 5 )
+        .attr("cy", 7.5)
+        .style("fill", function(d) {
+          return colors[d];})
 });
 // move outline and opacity to css
